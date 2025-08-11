a563eda462fdbe722ad4d796de1b8bd858d21e6a
agustinnigrelli <agustinnigrelli@gmail.com>
2025-08-10 23:40:38 -0300

    Updated ping handler

--- api/handlers/ping.go
+++ api/handlers/ping.go
@@ -6,6 +6,16 @@ import (
 	"github.com/agustinnigrelli/sandbox/utils/web"
 )
 
-func Ping(w http.ResponseWriter, r *http.Request) {
+type Ping interface {
+	Ping(w http.ResponseWriter, r *http.Request)
+}
+
+type PingHandler struct{}
+
+func NewPingHandler() Ping {
+	return &PingHandler{}
+}
+
+func (h *PingHandler) Ping(w http.ResponseWriter, r *http.Request) {
 	web.JsonResponse(w, http.StatusOK, map[string]string{"message": "pong"})
 }
